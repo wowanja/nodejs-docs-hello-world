793731b15bceddeee62aaa4e588d2a2fa90f14b1
krawtschukwa19 <waldemar.krawtschuk@seppmed.de>
2017-06-15 12:51:06 +0200

    add Python script

---
+++ PythonScripts/parseUboot.py
@@ -0,0 +1,18 @@
+from junit_xml import TestSuite, TestCase
+
+tc1 = TestCase('Test  1', 'RS232 UART3')
+tc1.add_error_info('Test ERROR', None, 'error')
+
+tc2 = TestCase('Test  2', 'RS232 UART4')
+tc2.add_failure_info('Test FAILED', None, 'failure')
+
+tc3 = TestCase('Test  2', 'CAN')
+tc3.add_skipped_info('Test SKIPPED')
+
+test_cases = [
+    tc1, tc2, tc3   
+]
+ts = TestSuite('MAXD1N3A00247', test_cases)
+
+with open('reports/MAXD1N3A00247.xml', 'w') as f:
+    TestSuite.to_file(f, [ts], prettyprint=True)
